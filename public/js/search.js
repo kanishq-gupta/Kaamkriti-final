0736e28aa102ef63dead58ca58a48ed29b45ccb4
kanishq-gupta <153586892+kanishq-gupta@users.noreply.github.com>
2025-01-15 14:57:25 +0530

    Delete public directory

--- public/js/search.js
+++
@@ -1,9 +0,0 @@
-function handleSearch(event) {
-    event.preventDefault();
-    
-    const city = document.getElementById('city').value;
-    const profession = document.getElementById('profession').value;
-    
-    // Redirect to filter page with query parameters
-    window.location.href = `/workers-list.html?city=${encodeURIComponent(city)}&profession=${encodeURIComponent(profession)}`;
-}
